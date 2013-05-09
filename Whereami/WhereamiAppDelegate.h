c6111bbfb92b98eecf58775e92894a40d638e025
Daniel Buckle <danielbuckle31@gmail.com>
2013-05-09 17:57:23 +0100

    Initial Commit

---
+++ Whereami/WhereamiAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WhereamiAppDelegate.h
+//  Whereami
+//
+//  Created by Daniel Buckle on 09/05/2013.
+//  Copyright (c) 2013 Daniel Buckle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WhereamiAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) IBOutlet UIWindow *window;
+
+@end
---
+++ Whereami/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Whereami
+//
+//  Created by Daniel Buckle on 09/05/2013.
+//  Copyright (c) 2013 Daniel Buckle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, nil);
+    }
+}
